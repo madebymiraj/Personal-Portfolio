3ce787978e281e5aa2621e8c14c80a2fae7ebc0c
Miraj Patel <miraj@psmiraj.com>
2015-01-30 06:24:22 -0500

    Finished Contact Form

---
+++ js/form-ajax.js
@@ -0,0 +1,25 @@
+$('form.nl-form').on('submit', function(){
+	var that = $(this),
+		url = that.attr("action"),
+		type = that.attar("method"),
+		data {};
+		
+	that.find('[name]').each(function() {
+		var that = $(this),
+			name = that.attr('name'),
+			value = that.val();
+		
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response){
+			console.log(response);
+		}
+	});
+	
+	return false;
+});
